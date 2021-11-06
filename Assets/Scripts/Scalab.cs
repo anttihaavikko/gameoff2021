d7361f0b44a06be32ff39b4e21ced786b2e4f080
Antti Haavikko <antti.haavikko@gmail.com>
2021-11-06 15:32:56 +0200

    Add scalab walking animation

--- Assets/Scripts/Scalab.cs
+++ Assets/Scripts/Scalab.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class Scalab : MonoBehaviour
 {
+    [SerializeField] private Animator anim;
+    
+    private static readonly int Walking = Animator.StringToHash("walking");
+
     private void Start()
     {
         Invoke(nameof(Move), 2f);
@@ -14,7 +18,16 @@ public class Scalab : MonoBehaviour
     {
         var t = transform;
         var p = t.position;
-        Tweener.MoveTo(t, Vector3.zero.RandomOffset(2f), 0.7f, TweenEasings.SineEaseInOut);
+        var target = Vector3.zero.RandomOffset(1.5f);
+        SetWalking(true, Vector3.Distance(p, target) * 0.9f);
+        Tweener.MoveTo(t, target, 0.7f, TweenEasings.SineEaseInOut);
+        this.StartCoroutine(() => SetWalking(false), 0.6f);
         Invoke(nameof(Move), 3f);
+    }
+
+    private void SetWalking(bool state, float speed = 1f)
+    {
+        anim.speed = speed;
+        anim.SetBool(Walking, state);
     }
 }
