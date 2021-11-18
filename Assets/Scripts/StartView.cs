9c1d28fb9fb472966b179888956b6278e7e8cf5d
Antti Haavikko <antti.haavikko@gmail.com>
2021-11-18 07:59:23 +0200

    Add restart button

--- Assets/Scripts/StartView.cs
+++ Assets/Scripts/StartView.cs
@@ -1,12 +1,31 @@
+using System;
 using AnttiStarterKit.Utils;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StartView : MonoBehaviour
 {
-    public void OnStart()
+    [SerializeField] private GameObject restartButton;
+    [SerializeField] private TMP_Text startText;
+
+    private void Start()
+    {
+        if (!Saver.Exists()) return;
+        restartButton.SetActive(true);
+        startText.text = "Continue";
+    }
+
+    public void Continue()
     {
         var scene = PlayerPrefs.HasKey("PlayerName") ? "Main" : "Name";
         SceneChanger.Instance.ChangeScene(scene);
+    }
+
+    public void Restart()
+    {
+        Saver.Clear();
+        Continue();
     }
 
     public void Quit()
