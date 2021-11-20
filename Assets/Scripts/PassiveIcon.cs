4cb764ef92fee3d99ea962fec09727561171b829
Antti Haavikko <antti.haavikko@gmail.com>
2021-11-20 20:31:51 +0200

    Add system for passive icons

--- Assets/Scripts/PassiveIcon.cs
+++ Assets/Scripts/PassiveIcon.cs
@@ -1,13 +1,17 @@
 using System;
+using AnttiStarterKit.ScriptableObjects;
 using Save;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PassiveIcon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private TMP_Text letter;
-
+    [SerializeField] private SpriteCollection sprites;
+    [SerializeField] private Image icon, iconShadow;
+    
     private PassiveDetails details;
     private PassiveTooltip tooltip;
     private int level;
@@ -18,6 +22,7 @@ public class PassiveIcon : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         details = Passives.GetDetails(passive);
         letter.text = details.name.Substring(0, 1);
         level = lvl;
+        icon.sprite = iconShadow.sprite = sprites.Get((int)passive);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
