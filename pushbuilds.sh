2fb40133898503f885c88890b4f8c2bfedd6c50a
Antti Haavikko <antti.haavikko@gmail.com>
2021-11-30 18:55:29 +0200

    Remove special characters from name

--- pushbuilds.sh
+++ pushbuilds.sh
@@ -13,6 +13,6 @@ echo "${COLOR}Pushing build for Linux${NC}"
 butler push Builds/linux anttihaavikko/domino:linux
 
 echo "${COLOR}Copying html5 files over to correct path"
-cp -a Builds/webgl/html5/Build/. Builds/html5/Build
+cp -a Builds/webgl/do-min-0/Build/. Builds/html5/Build
 echo "${COLOR}Pushing build for HTML5${NC}"
 butler push Builds/html5 anttihaavikko/domino:html5
